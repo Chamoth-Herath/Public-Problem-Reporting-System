36b8ab6d87888bf31dd5af2fb466d14739154b9a
Aloka Dewranga <cod920308@gmail.com>
2026-04-27 02:44:23 +0530

    Navigation Bar Commit (First Commit)

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import emblem from '../assets/emblem.png';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+
+            {/* LEFT */}
+            <div className="navbar-left">
+                <div className="navbar-logo">
+                    <img src={emblem} alt="Sri Lanka Emblem" className="emblem-img" />
+                    <span>Public Problem Reporting System</span>
+                </div>
+            </div>
+
+            {/* CENTER */}
+            <div className="navbar-center">
+                <ul className="navbar-links">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/services">Services</Link></li>
+                    <li><Link to="/gallery">Gallery</Link></li>
+                </ul>
+            </div>
+
+            {/* RIGHT */}
+            <div className="navbar-right">
+                <Link to="/signup" className="btn-signup">Signup</Link>
+                <Link to="/signin" className="btn-signin">Signin</Link>
+            </div>
+
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div style={{ padding: '40px', textAlign: 'center' }}>
+            <h1>Welcome to Civic Portal</h1> <br/>
+            <h1>Welcome to Civic Portal</h1> <br/>
+            <h1>Welcome to Civic Portal</h1> <br/>
+            <h1>Welcome to Civic Portal</h1> <br/>
+            <h1>Welcome to Civic Portal</h1> <br/>
+            <h1>Welcome to Civic Portal</h1> <br/>
+            <h1>Welcome to Civic Portal</h1> <br/>
+
+
+
+
+        </div>
+    );
+};
+
+export default Home;
