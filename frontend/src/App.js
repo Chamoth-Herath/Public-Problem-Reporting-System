8c216c419b2b126ab27f1da7d92fab7fcac195e8
Aloka Dewranga <cod920308@gmail.com>
2026-04-29 12:27:12 +0530

    commit with app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import DepartmentPage from './pages/DepartmentPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Services from './pages/Services';
 import Departments from './pages/Departments';
-import About from './pages/About';
 
 function App() {
     return (
@@ -16,7 +16,7 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/departments" element={<Departments />} />
                     <Route path="/services" element={<Services />} />
-                    <Route path="/about" element={<About />} />
+                    <Route path="/report/:dept" element={<DepartmentPage />} />
                 </Routes>
             </main>
             <Footer />
